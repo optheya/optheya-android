49181a0b401d55497bd62b973a2cc38d5cf8f34e
bersoncrios <bersondev@gmail.com>
2022-10-18 10:19:30 -0300

    add graph to ar

---
+++ app/src/main/java/com/br/optheya_android/screens/ar/ArMenu.kt
@@ -0,0 +1,12 @@
+package com.br.optheya_android.screens.ar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.br.optheya_android.R
+
+class ArMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ar_menu)
+    }
+}
